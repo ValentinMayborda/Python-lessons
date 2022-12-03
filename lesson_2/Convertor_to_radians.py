6d0eeca700792aa9c6224d60940236bd3cd6b123
Val <valentin.mayboroda@gmail.com>
2022-12-03 19:14:48 +0200

    Програма конвертує градуси в радіани.

--- lesson_2/Convertor_to_radians.py
+++ lesson_2/Convertor_to_radians.py
@@ -1,6 +1,8 @@
 import math
 
 print("Програма конвертує градуси в радіани")
+
 degrees = int(input('Введіть кількість градусів:'))
 radians = round(degrees * math.pi / 180, 5)
+
 print(f'{degrees} градусів = {radians} радіанів')
