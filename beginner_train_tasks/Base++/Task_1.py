af5df09be09076bdc6c3e232282dac9ec6ad748c
Valentin <valentin.mayboroda@gmail.com>
2022-12-14 12:23:51 +0200

    Завдання №1

---
+++ beginner_train_tasks/Base++/Task_1.py
@@ -0,0 +1,17 @@
+# Програма будує трикутник Паскаля розміром який введено.
+print('Програма будує трикутник Паскаля розміром який введено')
+n = int(input('Введіть розмір трикутника: '))
+
+list1 = []
+for i in range(n):
+    temp_list = []
+    for j in range(i + 1):
+        if j == 0 or j == i:
+            temp_list.append(1)
+        else:
+            temp_list.append(list1[i - 1][j - 1] + list1[i - 1][j])
+
+    list1.append(temp_list)
+
+for elem in list1:
+    print(*elem)
