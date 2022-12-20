68a9bb36535e4ee1a58aa36e48e6adbca0240b33
Valentin <valentin.mayboroda@gmail.com>
2022-12-20 16:53:44 +0200

    Завдання №4

--- beginner_train_tasks/Base+/Task_4.py
+++ beginner_train_tasks/Base+/Task_4.py
@@ -3,47 +3,36 @@
 day = int(input('Введіть дату: '))
 month = int(input('Введіть місяць: '))
 year = int(input('Введіть рік: '))
+year_intercalary = False
 
-if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):  # Якщо високосний рік
-    # якщо в місяці  31 день
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-        if 1 <= day <= 31:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
-    # якщо в місяці  30 днів
-    elif month == 4 or month == 6 or month == 9 or month == 11:
-        if 1 <= day <= 30:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
-    # якщо в місяці  29 днів
-    elif month == 2:
-        if 1 <= day <= 29:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
+# Якщо високосний рік
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    year_intercalary = True
+
+# якщо в місяці  31 день
+if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    if 1 <= day <= 31:
+        print('Така дата існує')
     else:
-        print('Такого місяця не існує!!!')
-# Якщо звичайний рік
+        print('Невірна дата!!!')
+# якщо в місяці  30 днів
+elif month == 4 or month == 6 or month == 9 or month == 11:
+    if 1 <= day <= 30:
+        print('Така дата існує')
+    else:
+        print('Невірна дата!!!')
+# якщо в місяці  29 днів у високосний рік
+elif month == 2 and year_intercalary:
+    if 1 <= day <= 29:
+        print('Така дата існує')
+    else:
+        print('Невірна дата!!!')
+
+# якщо в місяці  28 днів у звичайни рік
+elif month == 2 and not year_intercalary:
+    if 1 <= day <= 28:
+        print('Така дата існує')
+    else:
+        print('Невірна дата!!!')
 else:
-    # якщо в місяці  31 день
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-        if 1 <= day <= 31:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
-    # якщо в місяці  30 днів
-    elif month == 4 or month == 6 or month == 9 or month == 11:
-        if 1 <= day <= 30:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
-    # якщо в місяці  28 днів
-    elif month == 2:
-        if 1 <= day <= 28:
-            print('Така дата існує')
-        else:
-            print('Невірна дата!!!')
-    else:
-        print('Такого місяця не існує!!!')
+    print('Такого місяця не існує!!!')
