71a87c3662e67688e3fef05927337c16ff7521fb
Val <valentin.mayboroda@gmail.com>
2023-01-14 22:39:14 +0200

    Завдання на використання list comprehension

--- lesson_9/data_comprehension.py
+++ lesson_9/data_comprehension.py
@@ -1,12 +1,11 @@
-print("""Написати програму що читає текстовий файл та, використовуючи тільки list
-comprehension і ніякі інше цикли, розділяє текст на лінії (строки) методом readlines()
-та створює список таких строк. В кожній строці мають бути застосовані такі перетворення в такому порядку:""")
+print("""Программа читає текстовий файл та використовуює тільки list comprehension і ніякі інше цикли,
+розділяє текст на лінії (строки) методом readlines() та створює список.""")
 
 if __name__ == '__main__':
 
     with open('file.txt', mode='r', encoding='utf-8') as f:
         lst = f.readlines()
 
-    my_list = [elem.strip()[elem.find('a'):].capitalize() for elem in lst if 'a' in elem]
-
-    print(my_list)
+    my_list = [elem.strip()[elem.find('a'):].capitalize() if 'a' in elem else '' for elem in lst]
+    print()
+    print(f'Вихідний список: {my_list}')
