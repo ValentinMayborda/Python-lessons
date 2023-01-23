a047390260c82ccfb33eda7a835f92ca5eb85016
Valentin <valentin.mayboroda@gmail.com>
2023-01-23 17:24:48 +0200

    Функція -генератор послідовності чисел Фібоначчі.

---
+++ lesson_10/Task_1_fibonachi_generator/fibonachi_gen.py
@@ -0,0 +1,29 @@
+def fibonachi_gen(_n: int):
+    """
+    Функція генератор послідовності Фібаначі
+    :param _n: кількість елементів послідовності Фібаначі які треба згенерувати
+    :return: генерує значення послідовності Фібаначі
+    """
+    a = 0
+    b = 1
+    c = 1
+    for i in range(_n):
+        yield a
+        a = b
+        b = c
+        c = a + b
+
+
+if __name__ == '__main__':
+
+    while True:
+        try:
+            n = int(input('Скільки елементів послідовності Фібоначі вивести?: '))
+        except Exception:
+            print('Введіть цифрове значення!')
+            continue
+
+        for elem in fibonachi_gen(n):
+            print(elem)
+
+        exit(0)
