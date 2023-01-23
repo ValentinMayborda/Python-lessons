2a59d0dfc2bec23b2019421b4f3f6b86d8ef217a
Val <valentin.mayboroda@gmail.com>
2023-01-23 22:05:03 +0200

    Вправи з dict та csv файлами, створеня індексу

--- lesson_11/exercises_with_dict.py
+++ lesson_11/exercises_with_dict.py
@@ -9,9 +9,14 @@ try:
         - category
         - brand
         - price"""
+        lst_brand = list()
         for row in reader:
-
             print(row['model'], row['category'], row['brand'], row['price'])
+            lst_brand.append(row['brand'])
+
+        set_brand = set(lst_brand)
+        print(set_brand)
+
 except FileNotFoundError:
     print('Файл не знайдено!')
 except:
