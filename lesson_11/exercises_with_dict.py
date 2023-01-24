058fac2edd4999239b74709fab80237b8b28c622
Valentin <valentin.mayboroda@gmail.com>
2023-01-24 14:43:25 +0200

    Вправи з dict та csv файлами, створеня індексу

--- lesson_11/exercises_with_dict.py
+++ lesson_11/exercises_with_dict.py
@@ -1,4 +1,11 @@
 import csv
+from uuid import uuid4
+
+
+# Словарь де зберігаються всі записи з файлу
+data = {
+        'data': list()
+    }
 
 try:
     with open('tech_inventory.csv', newline='') as csvfile:
@@ -9,13 +16,31 @@ try:
         - category
         - brand
         - price"""
-        lst_brand = list()
+        lst_brand = list()  # список брендів
         for row in reader:
-            print(row['model'], row['category'], row['brand'], row['price'])
+            # print(row['model'], row['category'], row['brand'], row['price'])
+
             lst_brand.append(row['brand'])
 
-        set_brand = set(lst_brand)
-        print(set_brand)
+            data['data'].append(row)
+
+        #  створює індекс унікальних айді для кожного запису, тобто словник,
+        #  де ключі - це згенеровані унікальні айді, а значення - повна інформація про позицію товару
+        unique_index = dict()
+        for row in data['data']:
+            # створює унікальний айді
+            _uid = uuid4()
+            # зберігаєм під уникальним айді повні данні про запис
+            unique_index[_uid] = row['model'], row['category'], row['brand'], row['price']
+
+        # print(unique_index)
+        i = 1
+        for key, value in unique_index.items():
+            print(f'{i}. {key} : {value}')
+            i += 1
+
+
+        # print(data)
 
 except FileNotFoundError:
     print('Файл не знайдено!')
