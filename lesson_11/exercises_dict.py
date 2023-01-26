909a4fe570b1d2481eb299eba2f0b3d160ee3e02
Valentin <valentin.mayboroda@gmail.com>
2023-01-26 12:59:39 +0200

    Вправи на словарь

--- lesson_11/exercises_dict.py
+++ lesson_11/exercises_dict.py
@@ -2,6 +2,51 @@ import csv
 from uuid import uuid4
 
 
+def create_index(dictionary: dict, index_key: str) -> dict:
+    """
+    Создаёт индекс по точному совпадению
+    :param dictionary: индекс уникальных айдишников
+    :param index_key: ключ по которому создавать индекс
+    :return:
+    """
+    new_index = dict()
+    for uid, value in dictionary.items():
+        # если это значение уже есть в индексе, просто добавляем
+        # например, представителей должности Инженер еще не было, а Директор - уже был
+        if value[index_key] in new_index:
+            new_index[value[index_key]].append(uid)
+        # иначе, создаём список под это значение
+        else:
+            new_index[value[index_key]] = [uid]
+    return new_index
+
+# unique_index, brand_dct, gategory_name
+def positions_in_view(
+        unique_index_: dict, brand_dct_: dict,
+        gategory_name_: str, debug: bool = False
+) -> dict:
+    """
+    Функция считает сколько должностей работает в запрашиваемом отделе
+    В возвращаемом словаре будут только те должности, которые есть в отделе, а не все допустимые
+    :param unique_index_: индекс сотрудников
+    :param brand_dct_: индекс отделов
+    :param department_name: имя запрашиваемого отдела
+    :param debug: флаг для проверки корректной работы
+    :return: словарь, где ключи - представленные в отделе должности
+                        значения - их популяция
+    """
+    positions_in = dict()
+    for uid in brand_dct_[gategory_name_]:
+        if debug:
+            print(unique_index_[uid])
+        brand = unique_index_[uid]['brand']
+        if brand in positions_in:
+            positions_in[brand] += 1
+        else:
+            positions_in[brand] = 1
+    return positions_in
+
+
 if __name__ == '__main__':
     # Словник для зберігання данних з основного файлу
     data = {
@@ -34,4 +79,24 @@ if __name__ == '__main__':
     # створює індекс по категоріям та брендам. Тобто словник,
     # де ключі - це назва категорії/бренду, а значення - це перелік унікальних айді товарів,
     # в яких є таке значення поля категорії/бренду
-    category_brand_dct = dict()
+
+    # Виводить на екран статистику скільки товарів є від кожного бренда та від кожної категорії
+    category_dct = create_index(unique_index, 'category')
+
+    for key, value in category_dct.items():
+        print(f'В категорії {key} є {len(value)} одиниць')
+    print('*' * 200)
+
+    brand_dct = create_index(unique_index, 'brand')
+    for key, value in brand_dct.items():
+        print(f'В бренді {key} є {len(value)} одиниць')
+
+
+    # рахує розподіл товарів по брендам для кожної категорії та виводить це на екран.
+    # Наприклад, в категорії Ноутбуки представлено 6 товарів від Lenovo, 8 від Mac, 10 від Dell, тощо.
+
+    for gategory_name in brand_dct.keys():
+        print(
+            f'В категорії {gategory_name} ',
+            positions_in_view(unique_index, brand_dct, gategory_name)
+        )
